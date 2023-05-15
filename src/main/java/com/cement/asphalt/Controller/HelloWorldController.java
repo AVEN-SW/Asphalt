1f41059ee81744598f66fe7dec3a5e429697e9a8
SNOW <kancth03@gmail.com>
2023-05-16 05:30:27 +0900

    fix: api url 수정

--- src/main/java/com/cement/asphalt/Controller/HelloWorldController.java
+++ src/main/java/com/cement/asphalt/Controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-        @GetMapping("/api/hello")
+        @GetMapping("/hello")
         public String test() {
             return "Hello, world!";
         }
