0669c9e487d32a59c193403d8ddd798ab68c21f0
kimcurly <sanbonai0728@gimhyeonjun-ui-MacBookAir.local>
2023-05-17 01:17:24 +0900

    feat: 서버 연동 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
 // src/main/frontend/src/App.js
 
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-    const [hello, setHello] = useState('')
+  const [hello, setHello] = useState("");
 
-    useEffect(() => {
-        axios.get('/hello')
-            .then(response => setHello(response.data))
-            .catch(error => console.log(error))
-    }, []);
+  useEffect(() => {
+    // axios
+    //   .get("/hello")
+    //   .then((response) => setHello(response.data))
+    //   .catch((error) => console.log(error));
+    const fetchServer = async () => {
+      try {
+        const response = await axios.get("/hello");
+        console.log(response);
+        setHello(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchServer();
+  }, []);
 
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-    );
+  return <div>백엔드에서 가져온 데이터입니다 : {hello}</div>;
 }
 
 export default App;
