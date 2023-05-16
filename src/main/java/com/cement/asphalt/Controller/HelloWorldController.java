97cd48d06d5cb9cd0f07e38c88a18c362cf01bc2
SNOW <kancth03@gmail.com>
2023-05-17 02:20:38 +0900

    feat: swagger page 추가

--- src/main/java/com/cement/asphalt/Controller/HelloWorldController.java
+++ src/main/java/com/cement/asphalt/Controller/HelloWorldController.java
@@ -1,17 +1,26 @@
 package com.cement.asphalt.Controller;
 
 import dto.PersonDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 // Controller/HelloWorldController.java
+@Tag(name = "인증", description = "인증 관련 api 입니다.")
 @RestController
 public class HelloWorldController {
+
 
         @GetMapping("/hello")
         public String test() {
             return "Test";
         }
 
+        @Operation(summary = "검색 메서드", description = "현준이의 여자친구를 만들어주는 메서드")
         @PostMapping("/search")
         public String search(@RequestBody PersonDto personDto) {
             return "현준이가 사랑하는 " + personDto.getName();
